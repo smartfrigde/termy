08b6f63cce7648ee404fe67ebc4cdd80a1a1a991
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2025-05-07 12:31:28 +0200

    feat: persistent stores

--- core/store.ts
+++ core/store.ts
@@ -1,10 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
 import { authSlice } from './slices/authSlice';
 import { sshSlice } from './slices/sshSlice';
-
-export const store = configureStore({
-  reducer: {
+const rootReducer = combineReducers({
     auth: authSlice.reducer,
     ssh: sshSlice.reducer
-  },
-});
+  })
+
+  const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+  };
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  export const store = configureStore({
+    reducer: persistedReducer,
+  });
+  export const persistor = persistStore(store);
