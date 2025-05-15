ae05450323bbd8e7f442e35a5c8bb3b449e6f369
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2025-05-15 19:11:45 +0200

    fix: use correct TextInput component

--- components/modalViews/CreateServerView.tsx
+++ components/modalViews/CreateServerView.tsx
@@ -14,7 +14,7 @@ interface ServerViewProps {
 }
 
 function CTextInput(props: React.ComponentProps<typeof TextInput>) {
-    const InputComponent = isMobile() ? TextInput : BottomSheetTextInput;
+    const InputComponent = isMobile() ? BottomSheetTextInput: TextInput;
     return <InputComponent {...props} />;
 }
 
