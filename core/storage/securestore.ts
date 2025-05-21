d241f2500ff74159c3f2fef65731e5741570ab0e
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2025-05-21 08:50:37 +0200

    feat: use securestore on mobile

---
+++ core/storage/securestore.ts
@@ -0,0 +1,32 @@
+import * as SecureStore from "expo-secure-store";
+
+export interface ReduxPersistExpoSecureStore {
+  getItem(key: string): Promise<string | null>;
+  setItem(key: string, value: string): Promise<void>;
+  removeItem(key: string): Promise<void>;
+}
+
+interface CustomSecureStoreOptions extends SecureStore.SecureStoreOptions {
+  replaceCharacter?: string;
+  replacer?: (key: string, replaceCharacter: string) => string;
+}
+
+export default function createSecureStorage(
+  options: CustomSecureStoreOptions = {}
+): ReduxPersistExpoSecureStore {
+  const replaceCharacter = options.replaceCharacter || "_";
+  const replacer = options.replacer || defaultReplacer;
+
+  return {
+    getItem: (key: string) =>
+      SecureStore.getItemAsync(replacer(key, replaceCharacter), options),
+    setItem: (key: string, value: string) =>
+      SecureStore.setItemAsync(replacer(key, replaceCharacter), value, options),
+    removeItem: (key: string) =>
+      SecureStore.deleteItemAsync(replacer(key, replaceCharacter), options),
+  };
+}
+
+function defaultReplacer(key: string, replaceCharacter: string): string {
+  return key.replace(/[^a-z0-9.\-_]/gi, replaceCharacter);
+}
